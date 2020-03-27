19169ba58d6a8f86d0c9d53b04106520eb78d9cd
Dennis Frey <dennismfrey@gmail.com>
2020-03-26 20:20:10 -0400

    Added GuessingGame

---
+++ ch05_Methods/src/MethodsToMyMadnessDataValidation.java
@@ -0,0 +1,86 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MethodsToMyMadnessDataValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+
+		String name = "Dennis";
+
+		// call welcome methods
+		printWelcomeMessage();
+		printWelcomeMessage(name);
+		printWelcomeMessage("Roger Waters");
+
+		// System.out.println("Enter 2 integers (ex 1 5): ");
+		int a = getValidInt("Enter number 1 ", sc);
+		int b = getValidInt("Enter number 2 ", sc);
+
+		// get int a
+
+		// get int b
+
+		System.out.println("Sum is " + sum(a, b));
+		System.out.println("Difference is " + diff(a, b));
+		System.out.println("Quotient is " + quotient(a, b));
+		System.out.println("Product is " + product(a, b));
+
+		sc.close();
+		System.out.println("Bye");
+	}
+
+	private static int getValidInt(String prompt, Scanner sc) {
+
+		int i = 0;
+		while (true) {
+			// print message to the user
+			System.out.print(prompt);
+			// get user input with data validation
+			if (sc.hasNextInt()) {
+				i = sc.nextInt();
+				break;
+			} else {
+				// re-prompt if bad data
+				System.out.println("Error! Entry must be a whole number!");
+				sc.nextLine(); //discard bad data
+			}
+		}
+		// return valid input
+		return i;
+
+	}
+
+	private static void printWelcomeMessage() {
+		// private - cannot be called from other classes
+		// void is the return type
+		// no parameter
+		System.out.println("Welcome to my madness!");
+	}
+
+	private static void printWelcomeMessage(String name) {
+		// parameter string input
+		System.out.println("Welcome, " + name + ", to my madness!");
+
+	}
+
+	private static int sum(int n1, int n2) {
+		return n1 + n2;
+
+	}
+
+	// auto generated method using eclipse - highlight formula - right click -
+	// refactor - extract method
+	private static int diff(int a, int b) {
+		return a - b;
+	}
+
+	private static int quotient(int a, int b) {
+		return a / b;
+	}
+
+	private static int product(int a, int b) {
+		return a * b;
+	}
+}
---
+++ ch05_prj5-3_GuessingGame/src/GuessingGameApp.java
@@ -0,0 +1,138 @@
+import java.util.Scanner;
+
+public class GuessingGameApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Guess the Number Game\n" + "++++++++++++++++++++++++++++++++++++");
+
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		final int LIMIT = 100;
+		int count = 0;
+		
+
+		
+		while (choice.equalsIgnoreCase("y")) {
+
+			double d = Math.random() * LIMIT;
+			int number = (int) d;
+			number++;
+			int guess = 0;
+			boolean isCorrect = false;
+
+			prompt();
+			
+			while(!isCorrect) {
+				
+				guess = getIntWithinRange(sc, "Enter number: ", 1, 100);
+				isCorrect = processGuess(guess, number);			
+				count++;
+			}
+
+
+			numberOfAttemptsMsg(count);		
+			choice = tryAgain(sc);
+		}
+
+		sc.close();
+		System.out.println("\n Bye - Come back soon!");
+
+	}
+	
+	public static boolean processGuess(int guess, int number) {
+		
+	boolean correctGuess = false;
+	
+		if (guess == number) {
+			correctGuess = true;
+		}
+		else if (guess > number) {
+			if ((guess - number) > 10) {
+				System.out.println("Way too high! Guess again.\n");
+			}
+			else {
+				System.out.println("Too high! Guess again.\n");
+			}
+		}
+		else {
+			if ((number - guess) > 10) {
+				System.out.println("Way too low! Guess again.\n");
+			}
+			else {
+				System.out.println("Too low! Guess again.\n");
+			}
+		
+		}
+		
+		return correctGuess;
+	}
+
+	public static void prompt() {
+		System.out.println("\nI'm thinking of a number from 1 to 100.\n" + "Try to guess it.\n");
+	}
+
+	public static String tryAgain(Scanner sc) {
+
+		String choice = "";
+
+		while (true) {
+
+			System.out.print("\nTry again? (y/n) ");
+			choice = sc.next();
+			if (choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("n")) {
+				break;
+			} else {
+				// re-prompt if bad data
+				System.out.println("Error! Entry must be 'y' or 'n'. Try again");
+				sc.nextLine(); // discard bad data
+			}
+
+		}
+		return choice;
+	}
+
+	private static int getIntWithinRange(Scanner sc, String prompt, int min, int max) {
+
+		int i = 0;
+		while (true) {
+			// print message to the user
+			System.out.print(prompt);
+			// get user input with data validation
+			if (sc.hasNextInt()) {
+				i = sc.nextInt();
+				if (i >= 1 && i<=100) {
+					sc.nextLine(); //I was getting incorrect error output with valid guess followed by non int
+					break;
+				}
+				else {
+					System.out.println("Error - Enter a number between 1 to 100\n");
+					sc.nextLine(); //discard bad data
+				}
+			} else {
+				// re-prompt if bad data
+				System.out.println("Error! Entry must be a whole number!\n");
+				sc.nextLine(); //discard bad data
+			}
+		}
+		// return valid input
+		return i;
+
+	}
+	
+	public static void numberOfAttemptsMsg(int count) {
+		System.out.println("You got it in " + count + " tries." );
+		
+		if (count <= 3) {
+			System.out.println("Great work! You are a mathematical wizard.");
+		}
+		else if (count <= 7) {
+			System.out.println("Not too bad! You've got some potential.");
+		}
+		else {
+			System.out.println("What took you so long? Maybe you should take some lessons");
+		}
+			
+		
+	}
+}
